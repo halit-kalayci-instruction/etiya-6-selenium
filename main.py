49a5a899c03219ccc4e82be2e26520466802d691
halitkalayci <halit.kalayci@ext.etiya.com>
2023-05-31 17:49:45 +0300

    action chains

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.action_chains import ActionChains
+
 driver = webdriver.Chrome()
 #full-path url
 driver.get("https://www.etiya.com")
 driver.maximize_window()
-sleep(2)
+
+#defensive test
+# workshop görevi => bu kısmı fonksiyon haline getir. (derste de yapacağız.)
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "lang-select")))
 languageSelector = driver.find_element(By.CLASS_NAME, "lang-select")
-sleep(1)
 languageSelector.click()
-sleep(1)
+
+
 turkishLangugae = driver.find_element(By.XPATH, "//*[@id='etiya']/header/div/div[2]/div[1]/div[2]/div[1]/a")
-sleep(1)
 turkishLangugae.click()
-sleep(3)
 searchBtn = driver.find_element(By.ID, "search-btn")
-sleep(1)
 searchBtn.click()
-sleep(2)
 searchInput = driver.find_element(By.ID, "search-input")
-sleep(1)
 searchInput.send_keys("merhaba")
-sleep(1)
 searchIcon = driver.find_element(By.XPATH, "//*[@id='search-box']/form/div/button")
-sleep(1)
 searchIcon.click()
+
+## ActionChains
+driver.get("https://www.etiya.com")
+button = driver.find_element(By.XPATH,"//*[@id='home-leadform']/div[1]/div[1]/span[4]")
+actionChain = ActionChains(driver)
+actionChain.move_to_element(button)
+actionChain.click()
+actionChain.perform()
+## 
+
