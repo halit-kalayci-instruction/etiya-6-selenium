e19f553be0cb2133acbe301a171b04f47d1a4899
halitkalayci <halit.kalayci@ext.etiya.com>
2023-06-01 17:22:18 +0300

    latest

--- test_etiya.py
+++ test_etiya.py
@@ -8,16 +8,19 @@ import pytest
 class TestEtiya():
     #setup_method => pytest tarafından tanınan
     # bulunduğunda her test öncesi otomatik çalıştırılan fonk.
-    def setup_method(self):
+    def setup_method(self,method):
         #her test başlangıcında çalışacak fonksiyon
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.saucedemo.com/")
     #teardown_method
-    def teardown_method(self):
+    def teardown_method(self,method):
         #her test bitiminde çalışacak fonksiyon
         self.driver.quit()
-    @pytest.mark.skip
+
+    def readDataFromExcel():
+        print('x')
+
     def test_header(self):
         logo = self.driver.find_element(By.CLASS_NAME, 'login_logo')
         assert logo.text == "Swag Labs"
