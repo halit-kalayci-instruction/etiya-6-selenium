9b0ab00b151e76438ce1707a172b6f6c5b570099
halitkalayci <halit.kalayci@ext.etiya.com>
2023-06-01 17:11:20 +0300

    pytest

--- test_etiya.py
+++ test_etiya.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pytest
 #debugging
 #breakpoint => kodu debug modda iken bekleteceğimiz satır
+
+#@pytest.mark.skip
 class TestEtiya():
     #setup_method => pytest tarafından tanınan
     # bulunduğunda her test öncesi otomatik çalıştırılan fonk.
@@ -14,10 +17,22 @@ class TestEtiya():
     def teardown_method(self):
         #her test bitiminde çalışacak fonksiyon
         self.driver.quit()
+    @pytest.mark.skip
     def test_header(self):
         logo = self.driver.find_element(By.CLASS_NAME, 'login_logo')
         assert logo.text == "Swag Labs"
-    def test_login_invalid(self):
+
+    #bu test aynı anda 3 veriyle çalışsın.
+    # abc-123  deneme-12345  etiya-etiya
+    # annotation
+    @pytest.mark.parametrize("username,password",[("abc","123"), ("deneme","secret_sauce"), ("etiya","etiya")])
+    def test_login_invalid(self,username,password):
         loginBtn = self.driver.find_element(By.ID, 'login-button')
+        usernameInput = self.driver.find_element(By.ID, "user-name")
+        passwordInput = self.driver.find_element(By.ID,"password")
+        usernameInput.send_keys(username)
+        passwordInput.send_keys(password)
         loginBtn.click()
+        errorContainer = self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorContainer.text == "Epic sadface: Username and password do not match any user in this service"
 #prefix => ön ek
